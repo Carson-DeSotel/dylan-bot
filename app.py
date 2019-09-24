053f825b965ffa2a7442c63106203988687123a5
Carson DeSotel <carsonlouisd@gmail.com>
2019-09-24 09:52:19 -0500

    Change bot function

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def webhook():
   log('Recieved {}'.format(data))
 
   # We don't want to reply to ourselves!
-  if data['name'] != 'apnorton-test-bot':
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    send_message(msg)
+  if data['name'] != 'nice-groupme':
+    if('69' in data['text']):
+    send_message("nice")
 
   return "ok", 200
 
