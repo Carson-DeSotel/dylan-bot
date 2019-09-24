fda6576a1f02163de867b037a67a410af7c5bb5f
Carson DeSotel <carsonlouisd@gmail.com>
2019-09-24 11:08:40 -0500

    indentation fix

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def webhook():
     if('69' in data['text']):
         send_message("nice")
 
+    if('--beerme' in data['text']):
+        send_message("yeah bro, here, have a brewski")
+        upload_image_to_groupme("https://www.faustbrewing.com/images/beers/types/beerglassthree.png")
   return "ok", 200
 
 def send_message(msg):
@@ -30,6 +33,25 @@ def send_message(msg):
          }
   request = Request(url, urlencode(data).encode())
   json = urlopen(request).read().decode()
+
+def upload_image_to_groupme(imgURL):
+	imgRequest = requests.get(imgURL, stream=True)
+	filename = 'temp.png'
+	postImage = None
+	if imgRequest.status_code == 200:
+		# Save Image
+		with open(filename, 'wb') as image:
+			for chunk in imgRequest:
+				image.write(chunk)
+		# Send Image
+		headers = {'content-type': 'application/json'}
+		url = 'https://image.groupme.com/pictures'
+		files = {'file': open(filename, 'rb')}
+		payload = {'access_token': 'eo7JS8SGD49rKodcvUHPyFRnSWH1IVeZyOqUMrxU'}
+		r = requests.post(url, files=files, params=payload)
+		imageurl = r.json()['payload']['url']
+		os.remove(filename)
+		return imageurl
 
 def log(msg):
   print(str(msg))
